ccf7286e4ba7a8075489af653df7aab476963d6f
LuizGustavo048 <luiz.castico@tracevia.com.br>
2021-03-05 15:39:36 -0300

    Map Changes Equipments 15:39 05/03/2021

--- src/main/java/br/com/tracevia/webapp/dao/global/EquipmentsDAO.java
+++ src/main/java/br/com/tracevia/webapp/dao/global/EquipmentsDAO.java
@@ -342,8 +342,9 @@ public class EquipmentsDAO {
 					
 					SAT sat = new SAT();
 					
-					dir1 = translator.CheckDirection1(rs.getNString(7));
-					dir2 = translator.CheckDirection2(rs.getNString(7));
+					System.out.println(rs.getString(7));					
+					dir1 = translator.CheckDirection1(rs.getString(7));
+					dir2 = translator.CheckDirection2(rs.getString(7));
 					
 					sat.setEquip_id(rs.getInt(1));
 					sat.setTable_id("sat");
