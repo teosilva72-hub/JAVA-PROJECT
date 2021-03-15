4cead11dc7487f5b031e5be10da8a386ad142af0
Mateus Tracevia <representante.com.sp@gmail.com>
2021-03-15 14:16:16 -0300

    first one commit

--- src/main/webapp/resources/js/occ/controller_occ.js
+++ src/main/webapp/resources/js/occ/controller_occ.js
@@ -1,2 +1,16 @@
-var dados = document.querySelectorAll(".inputDesign");
-console.log("teste"+dados)
+var highway = document.getElementById("eventoRodovia").value;
+var state = document.getElementById("eventoEstado").value;
+
+
+
+
+    
+    if(highway == 1 && state == 4){
+        console.log("Rodovia "+ highway)
+        console.log("State: "+ state)
+    }else{
+	console.log("Erro")
+	}
+
+var x = new rodovia(highway, state);
+console.log(x)
