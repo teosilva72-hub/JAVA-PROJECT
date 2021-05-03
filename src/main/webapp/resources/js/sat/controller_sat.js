8ca1893a0276f0fb39ed734ee9201166f57ba7db
Mateus Tracevia <representante.com.sp@gmail.com>
2021-05-03 18:55:09 -0300

    night commit

--- src/main/webapp/resources/js/sat/controller_sat.js
+++ src/main/webapp/resources/js/sat/controller_sat.js
@@ -13,14 +13,36 @@ function page(page){
 	}
 }
 $(function(){
-	page(1);
-	
 	let pag = Math.ceil($("#page").children().length / content)
 	for(var i = 1; i <= pag; i++){
-		$("#btnPage").append(`<button type="button" onclick="page(${i})" class="btn btn-dark">${i}</button>`)
+		$("#btnPage").append(`
+		<label onclick="page(${i})" class="btn btn-dark">${i}
+			<input class="btn-check" type="radio" id="btn-page${i}">
+		</label>
+		`)
+		
+	}//paginação auto-incremento
+	$("#btnPage").children().first().click(); //initial btn
+	////////////////////////////////////////////////////////////////
+	for(var i = 1; i <= 10; i++){
+		
+		for(var a = 0; a <= 23; a++){
+			for(var b = 0; b <= 9; b++){
+				for(var c = 0; c <= 45; c += 15){
+					var d = Math.random()
+					if(d < 0.9){
+						$(`#sat${i} [col=${a < 10? '0' + a: a}] [row=${b}] [id=${c}]`).addClass("online")
+					}else if(d >= 0.9 && d < 0.98){
+						$(`#sat${i} [col=${a < 10? '0' + a: a}] [row=${b}] [id=${c}]`).addClass("warning")
+					}
+										
+				}
+					
+			}
+		}
 	}
 })
-
+//$("#sat2 [col=13] [row=5] [id=30]").addClass("online") //add info sat
 /*function btnEnable(){
 	$('[id$=btn-enable-user]').click();
 	eventValidator();
