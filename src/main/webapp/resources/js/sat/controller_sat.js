583f6ae89b6ad61d5951100f22833b16a6db4e74
Mateus Tracevia <representante.com.sp@gmail.com>
2021-05-05 18:56:09 -0300

    guigo commit

--- src/main/webapp/resources/js/sat/controller_sat.js
+++ src/main/webapp/resources/js/sat/controller_sat.js
@@ -29,20 +29,35 @@ $(window).on('load', function () {
     $('body').delay(250).css({'overflow': 'visible'});
 })
 $(function () {
-  $('[data-toggle="popover"]').popover({
-      html : true,
-      trigger: 'hover',
-      content: function() {
-        var content = $(this).attr("data-popover-content");
-        return $(content).children(".popover-body").html();
-      },
-      title: function() {
-        var title = $(this).attr("data-popover-content");
-        return $(title).children(".popover-header").html();
-      }
-  });
-
-  });
+	$('[data-toggle="popover"]').popover({
+	      html : true,
+	      trigger: 'hover',
+	      content: function() {
+	        var content = $(this).attr("data-popover-content");
+	        return $(content).children(".popover-body").html();
+	      },
+	      title: function() {
+	        var title = $(this).attr("data-popover-content");
+	        return $(title).children(".popover-header").html();
+	      }
+	
+	  	});
+		for(var i = 1; i < 50; i++){
+			let l = Math.random() * 7 + 2;
+			quantFaixas(i, parseInt(l)); //remove faixa
+		}
+		
+		$('.btn-sat').addClass('off');  //status sat
+		$('.btn-battery').addClass('off'); //status battery	
+});
+function quantFaixas(sat, faixas) {
+	if(faixas > 7) return;
+    faixas = Math.max(2, Math.min(8, faixas));
+    let query = ",";
+    for (let i = 8; i > faixas; i--)
+        query += `,[row=${i}]`;
+    $(`#sat${sat}`).find(query.replace(",,", "")).children().addClass("none");
+}
 //$("#sat2 [col=13] [row=5] [id=30]").addClass("online") //add info sat
 /*function btnEnable(){
 	$('[id$=btn-enable-user]').click();
