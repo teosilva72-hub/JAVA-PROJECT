39fab860c02edc3918f95ec2ad388db1060556c8
Mateus Tracevia <representante.com.sp@gmail.com>
2021-05-05 16:42:32 -0300

    mateus commit

--- src/main/java/br/com/tracevia/webapp/controller/sat/SatMaintenanceController.java
+++ src/main/java/br/com/tracevia/webapp/controller/sat/SatMaintenanceController.java
@@ -1,29 +1,21 @@
 package br.com.tracevia.webapp.controller.sat;
-
 import java.time.LocalDateTime;
 import java.util.concurrent.TimeUnit;
-
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import org.primefaces.context.RequestContext;
-
 @ManagedBean(name="maintenanceSatView")
 @ViewScoped
 public class SatMaintenanceController{
 private int hora;
 private String[] hours;
-
-
 public String[] getHours() {
 	return hours;
 }
-
 public void setHours(String[] hours) {
 	this.hours = hours;
 }
-
-
 	@PostConstruct
 	public void initializer() {
 		try {
@@ -34,28 +26,24 @@ public void setHours(String[] hours) {
 			e.printStackTrace();
 		}
 	}
-	public void enableUser() throws Exception {
-		TimeUnit.SECONDS.sleep(1);
-		RequestContext request = RequestContext.getCurrentInstance();
-		request.execute("btnEnable();");
-		hours();
-		
-	}//atualiza a tela
+	
 	public void hours() throws Exception{
-		
 		int hour = (hora + 1); 
 		hours = new String[24];
-
 		 for(int h = 0; h < 24; h++){
 			 if(hour > 23)
 		        hour = 0;
 			 	if(hour < 10)
 			 		hours[h] ="0" + hour;
-			 	
 			 	else hours[h] = String.valueOf(hour);
 			 	hour++;
-       
 		 }
+	}//horas sat
 	
-	}
+	/*public void enableUser() throws Exception {
+	TimeUnit.SECONDS.sleep(1);
+	RequestContext request = RequestContext.getCurrentInstance();
+	request.execute("btnEnable();");
+	hours();
+}//atualiza a tela*/
 }
--- src/main/webapp/resources/js/sat/controller_sat.js
+++ src/main/webapp/resources/js/sat/controller_sat.js
@@ -1,13 +1,11 @@
+//resolução
 var h = window.innerHeight |
  document.documentElement.clientHeight
  //document.body.clientHeight;
-
 var z = Math.floor((h - 160) / 168);
-
 var content = z * 2; /////quantidade de equipamento por página
 function page(page){
 	var x = $("#page").children().hide()
-	
 	for(var i = content * (page - 1); i < content * page; i++){
 		x.eq(i).show()
 	}
@@ -19,47 +17,32 @@ $(function(){
 		<label onclick="page(${i})" class="btn btn-dark">${i}
 			<input class="btn-check" type="radio" id="btn-page${i}">
 		</label>
-		`)
-		
+		`)	
 	}//paginação auto-incremento
 	$("#btnPage").children().first().click(); //initial btn
 	////////////////////////////////////////////////////////////////
 	//window.requestAnimationFrame(g)
-
 })
-var i = 1;
-/*function g(){
-	for(var a = 0; a <= 23; a++){
-			for(var b = 0; b <= 8; b++){
-				for(var c = 0; c <= 45; c += 15){
-					var d = Math.random()
-					var h = a < 10? '0' + a: a
-					if(d < 0.9){
-						document.querySelector(`#sat${i} [col="${h}"] [row="${b}"] [id="${c}"]`).classList.add("online")
-					}else if(d < 0.98){
-						//$(`#sat${i} [col=${a < 10? '0' + a: a}] [row=${b}] [id=${c}]`).addClass("warning")
-						document.querySelector(`#sat${i} [col="${h}"] [row="${b}"] [id="${c}"]`).classList.add("warning")
-
-					}
-										
-				}
-					
-			}
-		}
-		i++
-		if(i <= 49){
-			window.requestAnimationFrame(g)
-		}
-}*/
 $(window).on('load', function () {
     $('#preloader .inner').fadeOut();
-    $('#preloader').delay(400).fadeOut('slow'); 
-    $('body').delay(400).css({'overflow': 'visible'});
+    $('#preloader').delay(250).fadeOut('slow'); 
+    $('body').delay(250).css({'overflow': 'visible'});
 })
-
 $(function () {
-  $('[data-toggle="popover"]').popover()
-})
+  $('[data-toggle="popover"]').popover({
+      html : true,
+      trigger: 'hover',
+      content: function() {
+        var content = $(this).attr("data-popover-content");
+        return $(content).children(".popover-body").html();
+      },
+      title: function() {
+        var title = $(this).attr("data-popover-content");
+        return $(title).children(".popover-header").html();
+      }
+  });
+
+  });
 //$("#sat2 [col=13] [row=5] [id=30]").addClass("online") //add info sat
 /*function btnEnable(){
 	$('[id$=btn-enable-user]').click();
