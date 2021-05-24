a1f764e9e96366f7469713916d8c5f0a80d56fb9
Guilherme França de Oliveira <guigo613@gmail.com>
2021-05-24 10:09:55 -0300

    up fix

--- src/main/webapp/resources/js/realtime/realtime.js
+++ src/main/webapp/resources/js/realtime/realtime.js
@@ -13,8 +13,9 @@ setTimeout(() => {
 
 		if (minute == 1 || minute == 16 || minute == 31 || minute == 46) {
 			if (n < 4)
+				init();
 
-	location.href = location.protocol + '//' + location.host + location.pathname
+	// location.href = location.protocol + '//' + location.host + location.pathname
 }
 			     
 	}, 3000)
@@ -56,6 +57,7 @@ const init = () => {
 		setEquipToolTip();
 		showGenericName();
 		initPMV();
+		initSOS()
 		statusColors();
 	})
 }
@@ -234,8 +236,6 @@ $(function () {
 	$('#coefSize').change(function () {
 		resizeEquipScale($('[scroll-zoom]'))
 	})
-
-	initPMV();
 })
 
 function posReset() {
