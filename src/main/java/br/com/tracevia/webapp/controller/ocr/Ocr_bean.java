16ec18471d224d30d677a24027391eebc5c34b11
Mateus Tracevia <representante.com.sp@gmail.com>
2021-06-25 19:33:54 -0300

    z commit

--- src/main/java/br/com/tracevia/webapp/controller/ocr/Ocr_bean.java
+++ src/main/java/br/com/tracevia/webapp/controller/ocr/Ocr_bean.java
@@ -4,11 +4,27 @@ import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
+import br.com.tracevia.webapp.model.ocr.OcrData;
+
 @ManagedBean(name="ocrBean")
 @ViewScoped
 public class Ocr_bean {
 	private String[] dados;
 	private String path;
+	private OcrData data;
+	
+	public String getPath() {
+		return path;
+	}
+	public void setPath(String path) {
+		this.path = path;
+	}
+	public OcrData getData() {
+		return data;
+	}
+	public void setData(OcrData data) {
+		this.data = data;
+	}
 	public String[] getDados() {
 		return dados;
 	}
@@ -29,6 +45,6 @@ public class Ocr_bean {
 		dados [0] = "1525";
 		dados [1] = "2021-01-01 15:01:25";
 		dados [2] = "CAM3";
-		dados [3] = "CXD-0025";
+		dados [3] = "YKY-367-A";
 	}
 }
