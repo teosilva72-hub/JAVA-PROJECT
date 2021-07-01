7515ef273601062e75a1eb9d7ab851800c66926d
Mateus Tracevia <representante.com.sp@gmail.com>
2021-07-01 15:36:09 -0300

    leto commit

--- src/main/java/br/com/tracevia/webapp/controller/ocr/OcrReport.java
+++ src/main/java/br/com/tracevia/webapp/controller/ocr/OcrReport.java
@@ -1,16 +1,37 @@
 package br.com.tracevia.webapp.controller.ocr;
 
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 import javax.faces.model.SelectItem;
 
 import org.primefaces.context.RequestContext;
 
-import br.com.tracevia.webapp.dao.ocr.OCRDAO;
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.FontFactory;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.PageSize;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.Rectangle;
+import com.itextpdf.text.pdf.ColumnText;
+import com.itextpdf.text.pdf.PdfWriter;
+
+import br.com.tracevia.webapp.dao.ocr.reportDAO;
 import br.com.tracevia.webapp.model.ocr.OCR;
 
 @ViewScoped
@@ -18,18 +39,57 @@ import br.com.tracevia.webapp.model.ocr.OCR;
 public class OcrReport{
 	private OCR data = new OCR();
 	private OCR date;
-	private String dtStart, hrStart, minStart, dtFinal, hrFinal, minFinal, camera; 
+	private String dtStart, hrStart,
+	minStart, dtFinal, hrFinal,
+	minFinal, camera, img1 = "", img2 = "",
+	pasta = "C:\\teste\\" ;
+
 	private List<SelectItem> minutos, horas, classes;
 	private List<OCR> list;
 	private int rowkey;
 	private boolean selectedRow;
-	private OCRDAO dao = new OCRDAO();
-	
+	private reportDAO dao = new reportDAO();
+
+	public String getImg1() {
+		try {
+
+			Path path = Paths.get(img1);
+			byte[] file = Files.readAllBytes(path);
+			return Base64.getEncoder().encodeToString(file);
+		} catch (IOException e) {
+			return "";
+		}
+
+	}
+
+
+	public void setImg1(String img1) {
+		this.img1 = img1;
+	}
+
+
+	public String getImg2() {
+		try {
+
+			Path path = Paths.get(img2);
+			byte[] file = Files.readAllBytes(path);
+			return Base64.getEncoder().encodeToString(file);
+		} catch (IOException e) {
+			return "";
+		}
+	}
+
+
+	public void setImg2(String img2) {
+		this.img2 = img2;
+	}
+
+
 	public OCR getData() {
 		return data;
 	}
 
-	
+
 	public String getCamera() {
 		return camera;
 	}
@@ -178,10 +238,11 @@ public class OcrReport{
 	@PostConstruct
 	public void initialize() {
 		RequestContext.getCurrentInstance().execute("getTr()");
+		img1 = pasta+"no-image.png";
+		img2 = pasta+"no-image.png";
 		horas = new ArrayList<SelectItem>();
 		minutos = new ArrayList<SelectItem>();
 		classes = new ArrayList<SelectItem>();
-		classes.add(new SelectItem("Todos"));
 		classes.add(new SelectItem("ocr_1"));
 		classes.add(new SelectItem("ocr_2"));
 		classes.add(new SelectItem("ocr_3"));
@@ -200,16 +261,22 @@ public class OcrReport{
 				minutos.add(new SelectItem(String.valueOf(m), String.valueOf(m)));
 		}
 	}
-	public void search() {
-		
+	public String path() throws IOException {
+		return Base64.getEncoder().encodeToString(Files.readAllBytes(Paths.get("C:\\Users\\mateu\\Pictures\\bahianorte.jpg")));
+	}
+
+	public void search() throws IOException {
+		img1 = pasta+"no-image.png";
+		img2 = pasta+"no-image.png";
 		String start = dtStart+" "+ hrStart+":"+minStart;
 		String end = dtFinal+" "+ hrFinal+":"+minFinal;
+		if(camera == "Todos") camera ="";
 		String cam = camera;
-		System.out.println(start);
-		System.out.println(end);
-		if(start != null && end != null) {
-			
+
+		if(cam != "") {
+
 			try {
+				System.out.println("com classe");
 				list = dao.searchTable(start, end, cam);
 				RequestContext.getCurrentInstance().execute("getTr()");
 				RequestContext.getCurrentInstance().execute("dataPicker()");
@@ -217,7 +284,106 @@ public class OcrReport{
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+
+		}else {
+			System.out.println("sem classe");
+			String start2 = dtStart+" "+ hrStart+":"+minStart;
+			String end2 = dtFinal+" "+ hrFinal+":"+minFinal;
+			RequestContext.getCurrentInstance().execute("getTr()");
+			RequestContext.getCurrentInstance().execute("dataPicker()");
+			try {
+				list = dao.searchTable2(start2, end2);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	public void idGet() {
+		System.out.println(rowkey);
+		try {
+			data = dao.searchId(rowkey);
+			String dt = data.getDataHour();
+			dt = dt.replaceAll("-", "");
+			dt = dt.replaceAll(":", "");
+			dt = dt.replaceAll(" ", "");
+			String pl = data.getPlaca();
+			pl = pl.replaceAll("-", "");
+			pl = pl.replaceAll(" ", "");
+			img1 = pasta+data.getCam()+"_"+data.getDataHour()+"_"+pl+".jpg";
+			img2 = pasta+"Plate"+data.getCam()+"_"+data.getDataHour()+"_"+pl+".jpg";
+			System.out.println(img1);
+			System.out.println(img2);
 			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public void pdf() {
+		try {
+			date = dao.searchId(getRowkey());
+			String RESULT = "/teste/"+date.getId()+".pdf";
+			Document document = new Document();
+			FacesContext facesContext = FacesContext.getCurrentInstance();
+			ExternalContext externalContext = facesContext.getExternalContext();
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			PdfWriter writer = PdfWriter.getInstance(document, baos);
+			document.open();
+			document.setPageSize(PageSize.A4);
+			Paragraph pTitulo = new Paragraph(new Phrase(20F,"OCR REPORT"));
+			ColumnText tl = new ColumnText(writer.getDirectContent());
+			Paragraph tx = new Paragraph();
+			tl.setSimpleColumn(400,820,200,50);
+			tx.add(pTitulo);
+			tl.addElement(tx);
+			tl.go();
+			document.add(new Paragraph("\n\n"));
+			document.add(new Paragraph("ID: "+ date.getId()
+			+"\nData/Hour: " + date.getDataHour()
+			+"\nCâmera: "+ date.getCam()
+			+"\nPlaca: "+ date.getPlaca()));
+			Rectangle rowPage = new Rectangle(577, 40, 10, 790); //linha da pagina 
+
+			rowPage.setBorderColor(BaseColor.BLACK);
+			rowPage.setBorderWidth(2);
+			rowPage.setBorder(Rectangle.BOX);
+			document.add(rowPage);
+			//final da linda da pagina
+			ColumnText ct = new ColumnText(writer.getDirectContent());
+			ct.setSimpleColumn(700,0,200,30);
+			Paragraph p = new Paragraph();
+			p.add("                              "+"Pag 1");//paragrafo Evento
+			ct.addElement(p);
+			ct.go();
+
+			document.add(new Paragraph(""));
+
+
+			document.close();
+			FileOutputStream fos = new FileOutputStream(RESULT);
+			fos.write(baos.toByteArray());
+			fos.close();  
+			// DOWNLOAD
+
+			externalContext.setResponseContentType("application/pdf");
+			externalContext.setResponseHeader("Content-Disposition","attachment; filename=\""+"OCC.pdf\"");
+
+			externalContext.setResponseContentLength(baos.size());
+
+			OutputStream responseOutputStream = externalContext.getResponseOutputStream();  
+			baos.writeTo(responseOutputStream);
+			responseOutputStream.flush();
+			responseOutputStream.close();
+
+
+			facesContext.responseComplete();  
+
+			// DOWNLOAD
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 }
---
+++ src/main/java/br/com/tracevia/webapp/dao/ocr/reportDAO.java
@@ -0,0 +1,100 @@
+package br.com.tracevia.webapp.dao.ocr;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import br.com.tracevia.webapp.methods.DateTimeApplication;
+import br.com.tracevia.webapp.model.ocr.OCR;
+import br.com.tracevia.webapp.model.wim.WimData;
+import br.com.tracevia.webapp.util.ConnectionFactory;
+
+public class reportDAO {
+	private Connection conn;
+	private PreparedStatement ps, ps1;
+	private ResultSet rs, rs1;
+	OCR data = new OCR();
+	
+	public ArrayList<OCR> searchTable(String start, String end, String classe) throws Exception {
+		String search = "SELECT * FROM ocr_data WHERE datetime BETWEEN'"+ start +"'AND'"+ end +"'AND site_name ='"+ classe+"'";
+		String search1 = "SELECT * FROM ocr_data WHERE datetime BETWEEN'"+ start +"'AND'"+ end+"'";
+		ArrayList<OCR> list = new ArrayList<OCR>();
+		try {
+			conn = ConnectionFactory.connectToTraceviaApp();
+			ps = conn.prepareStatement(search);
+			rs = ps.executeQuery();
+			ps1 = conn.prepareStatement(search1);
+			rs1 = ps1.executeQuery();
+			if (rs.isBeforeFirst()) {
+				while (rs.next()) {
+					System.out.println("tem classe");
+					OCR data = new OCR();			
+					data.setId(rs.getString(1));
+					data.setCam(rs.getString(2));
+					data.setDataHour(rs.getString(3));
+					
+					data.setPlaca(rs.getString(4));
+					list.add(data);
+				}				
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return list;
+
+	}
+	public ArrayList<OCR> searchTable2(String start, String end) throws Exception {
+		String search = "SELECT * FROM ocr_data WHERE datetime BETWEEN'"+ start +"'AND'"+ end +"'";
+		ArrayList<OCR> list = new ArrayList<OCR>();
+		try {
+			conn = ConnectionFactory.connectToTraceviaApp();
+			ps = conn.prepareStatement(search);
+			rs = ps.executeQuery();
+			
+			if (rs.isBeforeFirst()) {
+				while (rs.next()) {
+					OCR data = new OCR();			
+					data.setId(rs.getString(1));
+					data.setCam(rs.getString(2));
+					data.setDataHour(rs.getString(3));
+					data.setPlaca(rs.getString(4));
+					list.add(data);
+				}				
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return list;
+
+	}
+	public OCR searchId(int id) throws Exception {
+		OCR data = new OCR();
+		String search = "SELECT * FROM ocr_data WHERE id_ocr_data ='"+ id +"'";
+		DateTimeApplication dtm = new DateTimeApplication();
+		try {
+			conn = ConnectionFactory.connectToTraceviaApp();
+			ps = conn.prepareStatement(search);
+			rs = ps.executeQuery();
+			if (rs.isBeforeFirst()) {
+				while (rs.next()) {
+					data.setId(rs.getString(1));
+					data.setCam(rs.getString(2));
+					data.setDataHour(rs.getString(3));
+					data.setPlaca(rs.getString(4));
+				}				
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return data;
+
+	}
+}
