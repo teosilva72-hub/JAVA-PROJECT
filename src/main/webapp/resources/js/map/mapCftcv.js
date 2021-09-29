915247a690a431b3e040b91d7463e670e41f8fa1
Mateus Tracevia <representante.com.sp@gmail.com>
2021-09-29 10:03:12 -0300

    commit one

--- src/main/webapp/resources/js/map/mapCftcv.js
+++ src/main/webapp/resources/js/map/mapCftcv.js
@@ -1,18 +1,25 @@
 function getInfo(){
 	$("#send-cftv-id").click();
+	var url = document.getElementById("url-img")
+	var x = document.querySelector(".video-img").src = url.value
+
 }
 function cftvVideo(){
+	
+	
 	var id = document.getElementById("cftvId").value
-		$("#dinamic-div-cftv").append("\
-		<div class=\"img-cftv-div bg-dark ptz"+id+"\">\
-				<p class=\"text-cftv-barra\">PTZ "+id+"</p>\
-				<i class=\"fas fa-times close-cftv ptz"+id+"\"></i>\
-				<img scr\"#{CftvCam.camCftv}\" class=\"video-img\"/>\
-		</div>")
+		$("#dinamic-div-cftv").append(`\
+		<div class="img-cftv-div bg-dark ptz${id}">\
+				<p class="text-cftv-barra">PTZ ${id}</p>\
+				<i class="fas fa-times close-cftv ptz${id}"></i>\
+				<img src="${url.value}" class="video-img"/>\
+		</div>`)
+		document.querySelector(".video-img").src = url.value
 		$(".img-cftv-div").fadeIn(3000);
-		//close(".ptz"+id)
+		close(".ptz"+id)
 		dragdrop(".ptz"+id)
 }
+
 function close(cam){
 	var click = $(cam)
 		click.click(function() {
