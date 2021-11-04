7c28c22e9f17f10c736f7369d4f99d5bfca733db
Wellington <developers.tracevia@gmail.com>
2021-11-04 15:24:21 -0300

    well lastChanges

--- src/main/webapp/resources/js/validation-forms/reports/global/global-validation.js
+++ src/main/webapp/resources/js/validation-forms/reports/global/global-validation.js
@@ -2,23 +2,29 @@
 
 $(function () {
 	
-	//Get equipments from backing bean
-    //Create array
-    var equipsArray = new Array();
+	let maxDays = 60; // Máximo de dias para pesquisa
+	
+	// Get equipments from backing bean
+    // Create array
+    let equipsArray = new Array();
    
-    var validation = document.forms.validation;
+    let validation = document.forms.validation;
 
     for (messages of validation)
         if (messages.name == "")
             equipsArray.push(messages.value);
+
        else window[messages.name] = messages.value
 
-    //Create / Define multiselect
-    defineMultiselect('#equips', equipmentSelectMsg);      
+     // Create / Define multiselect
+     defineMultiselect('#equips', equipmentSelectMsg);
 
-    $.validator.methods.date = function (value, element) {
-        //This is not ideal but Chrome passes dates through in ISO1901 format regardless of locale 
-        //and despite displaying in the specified format.
+    /* ----------------------------------------------------------------------------------------------------- */     
+
+    $.validator.addMethod("date", function (value, element) {
+       
+        // This is not ideal but Chrome passes dates through in ISO1901 format regardless of locale 
+        // and despite displaying in the specified format.
 
         let a = value.split("/");
         let date = new Date(a[2], a[1] - 1, a[0])
@@ -41,12 +47,66 @@ $(function () {
         let check = date.toLocaleDateString() == value && timestamp < Date.now() && range
 
         return this.optional(element) || check;
-    }
+    }, "foda-se");
+
+    /* ----------------------------------------------------------------------------------------------------- */ 
+
+     $.validator.addMethod("searchDays", function (value, element) {
+	
+	    let id = element.id
+
+		let max = maxDays;
+		
+		
+		  if (id.endsWith("-start"))
+		      let startDate = $(id).val();
+
+          else if(id.endsWith("-end"))
+		      let endDate = $(id).val();
+		
+		let dataInicio = toDate(startDate);
+		let dataFim = toDate(endDate);
+
+		let daysDiff = (dataFim - dataInicio) / (1000 * 3600 * 24);
+
+		if (daysDiff > max) {
+
+			$(dateEnd).removeClass('valid').addClass('invalid');
+			$(dateError).parent().find('.error').fadeIn(500);
+			return false;
+		}
+		else {
+			$(dateEnd).removeClass('invalid').addClass('valid'); return true;
+		}
+	}, "Fuck you");
+
+
+
 
     //Language
    $('.datepicker').datepickerLocale(language);
 
    validationTemplate9("#report-form", '#dateStart', requiredEquipmentsMsg, requiredDateStartMsg, requiredDateEndMsg, requiredValidDateMsg);
+
+  //Validate elements on change value
+    //In this case check if the element is valid
+    validateOnChange('#equip');
+    validateOnChange('#periods');
+    validateOnChange('#dateStart');
+    validateOnChange('#dateEnd');
+    
+	//Reset Fields on close modal
+	resetFieldOnModalClose('#modalReportOptions', 'equip');
+	resetFieldOnModalClose('#modalReportOptions', 'periods');
+    resetFieldOnModalClose('#modalReportOptions', 'dateStart');
+	resetFieldOnModalClose('#modalReportOptions', 'dateEnd');
+    
+    //Remove validation icons
+    //click reset button action		 
+    removeValidationIcon("reset-btn", 'equip');
+    removeValidationIcon("reset-btn", 'periods');
+    removeValidationIcon("reset-btn", 'dateStart');
+    removeValidationIcon("reset-btn", 'dateEnd');
 
 	//Clean Form validation on close modal
     cleanValidationOnModalClose("#report-form", '#modalReportOptions');
