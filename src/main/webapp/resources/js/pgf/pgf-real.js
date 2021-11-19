cc9f136620650c617d234d5ae71d76e0c8498830
Luiz Gustavo <luiz.castico@tracevia.com.br>
2021-11-19 11:47:05 -0300

    pgf changes 19/11/2021 11:46
    Co-authored-by: guigo613 <guigo613@users.noreply.github.com>

---
+++ src/main/webapp/resources/js/pgf/pgf-real.js
@@ -0,0 +1,6 @@
+ var adjustLists = function () {
+    $(".pgf-card").slimScroll({ height: window.innerHeight - 40 })
+    $("#pgf-tires").slimScroll({ height: window.innerHeight - 40 })
+      .css('height', window.innerHeight - 200)
+
+  };
