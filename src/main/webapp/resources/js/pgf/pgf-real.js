81a154eee4015a210c6e886eea0fb7784969dee0
Luiz Gustavo <luiz.castico@tracevia.com.br>
2021-11-19 13:09:20 -0300

    pgf change 19/11/2021 13:09

--- src/main/webapp/resources/js/pgf/pgf-real.js
+++ src/main/webapp/resources/js/pgf/pgf-real.js
@@ -1,6 +1,11 @@
  var adjustLists = function () {
-    $(".pgf-card").slimScroll({ height: window.innerHeight - 40 })
+    $(".pgf-card").slimScroll({ height: window.innerHeight - 328 })
     $("#pgf-tires").slimScroll({ height: window.innerHeight - 40 })
-      .css('height', window.innerHeight - 200)
+      .css('height', window.innerHeight - 150)
 
   };
+
+ adjustLists();
+  $(window).resize(function () {
+    adjustLists();
+  });
