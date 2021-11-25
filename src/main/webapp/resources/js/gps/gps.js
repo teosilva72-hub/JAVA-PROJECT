d08b3bc2f6432760906390b8f4b6ab51a40879a1
Tracevia Teste <developers.tracevia.test@gmail.com>
2021-11-25 14:40:55 -0300

    add gps
    Co-authored-by: luizgustavo212@hotmail.com <luizgustavo212@hotmail.com>

--- src/main/webapp/resources/js/gps/gps.js
+++ src/main/webapp/resources/js/gps/gps.js
@@ -6,15 +6,66 @@ const connectGPS = async (request, debug) => {
 
 const drawPoint = item => {
 	let divItem = draw.find(`#${item.i}`)
-	let i = $("<div>")
-	i.css({position: 'absolute', left: '100px', top: '100px', width: "50px", height: '50px', 'background-color': 'black'})
+	let start = draw.attr('gps_start').replaceAll(',', '.').split(';')
+	let end = draw.attr('gps_end').replaceAll(',', '.').split(';')
+	let pos = {
+		s1: Number(start[0]),
+		s2: Number(start[1]),
+		e1: Number(end[0]),
+		e2: Number(end[1]),
+		ps: Number(start[2]),
+		pe: Number(end[2]),
+		x: Number(item.d.pos.x),
+		y: Number(item.d.pos.y)
+	}
+	let distance = {
+		longitude: Math.abs(pos.e1 - pos.s1),
+		latitude: Math.abs(pos.e2 - pos.s2),
+		x: draw.width(),
+		y: Math.abs(pos.pe - pos.ps),
+		hypotenuse: () => Math.sqrt(Math.pow(distance.longitude, 2) + Math.pow(distance.latitude, 2)),
+		radius: () => Math.atan(distance.latitude / distance.longitude),
+		pixel: () => Math.sqrt(Math.pow(draw.width(), 2) + Math.pow(distance.y, 2)),
+		radiusPixel: () => Math.atan(distance.y / distance.x),
+	}
+	let diff = {
+		longitude: Math.abs(pos.x - pos.s1),
+		latitude: Math.abs(pos.y - pos.s2),
+		hypotenuse: () => Math.sqrt(Math.pow(diff.longitude, 2) + Math.pow(diff.latitude, 2)),
+		radius: () => Math.atan(diff.latitude / diff.longitude),
+		radiusDiff: () => distance.radius() - diff.radius() + distance.radiusPixel()
+	}
+	let percent = {
+		longitude: diff.longitude / distance.longitude,
+		latitude: diff.latitude / distance.latitude,
+		hypotenuse: diff.hypotenuse() / distance.hypotenuse()
+	}
+	let point = {
+		x: percent.hypotenuse * distance.pixel() * Math.cos(diff.radiusDiff()),
+		y: pos.ps - percent.hypotenuse * distance.pixel() * Math.sin(diff.radiusDiff()),
+	}
+	let outRange = point.x > draw.width() || point.y > draw.height() || pos.x < Math.min(pos.s1, pos.e1) - 0.1 || pos.x > Math.max(pos.s1, pos.e1) + 0.1 || pos.y < Math.min(pos.s2, pos.e2) - 0.1 || pos.y > Math.max(pos.s2, pos.e2) + 0.1
+	let defaultCss = {position: 'absolute', left: `${point.x}px`, top: `${point.y}px`, 'border-bottom': "5px solid transparent", 'border-top': '5px solid transparent', 'border-left': '5px solid red'}
+
 	if (divItem.length) {
-		divItem.empty().append(i)
-	} else {
-		let n = $(`<div id="${item.i}">`).append(i)
+		if (outRange)
+			divItem.remove()
+		else
+			divItem.css(defaultCss)
+	} else if (!outRange) {
+		let n = $(`<div id="${item.i}">`).css(defaultCss)
 		draw.append(n)
 	}
-	console.log(i)
+
+
+	console.log("id: " + item.i)
+	console.log(distance.radiusPixel())
+	console.log(distance.radius() - diff.radius())
+	console.log(diff.radiusDiff())
+	console.log(Math.sin(diff.radiusDiff()))
+	console.log(percent.hypotenuse * distance.pixel() * Math.sin(diff.radiusDiff()))
+	console.log(pos.ps - percent.hypotenuse * distance.pixel() * Math.sin(diff.radiusDiff()))
+	console.log('-' * 20)
 }
 
 const callback_gps_default = response => {
