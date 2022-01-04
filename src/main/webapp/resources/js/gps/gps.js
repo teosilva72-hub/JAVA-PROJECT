9b59c3ca4bd784985abc39b9a42ea89a749f1da4
Guilherme França de Oliveira <guigo613@gmail.com>
2022-01-04 12:54:33 -0300

    fix

--- src/main/webapp/resources/js/gps/gps.js
+++ src/main/webapp/resources/js/gps/gps.js
@@ -85,7 +85,7 @@ const openModalGPS = () => {
 const fillEquips = name => {
 	let zoom = $(`[name=${name}]`)
 	let radius = zoom.width() / 2
-	zoom.children().filter(':not(img)').remove()
+	zoom.children().filter(':not(img, .background-zoomPoint)').remove()
 	$('.equip-box, .equip-info, .equip-box-sat').each((idx, item) => {
 		item = $(item).clone()
 
@@ -145,7 +145,7 @@ const zoomRoadPoint = name => {
 	
 	let pos = posZoom[name]
 
-	zoom.css({
+	let width = zoom.css({
 		position: "absolute",
 		left: pos.start.x + (pos.end.x - pos.start.x) / 2,
 		top: pos.start.y + (pos.end.y - pos.start.y) / 2,
@@ -153,8 +153,11 @@ const zoomRoadPoint = name => {
 		display: "block",
 		overflow: "hidden",
 		"z-index": 1
+	}).children().eq(1).css("height", "100%").css(`width`)
+	zoom.children().first().css({
+		height: width,
+		width: width
 	})
-	zoom.children().eq(1).css("height", "100%")
 
 	fillEquips(name)
 }
