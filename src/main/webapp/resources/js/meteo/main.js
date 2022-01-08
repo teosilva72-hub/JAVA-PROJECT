a90d389119e4ef868a5d4d2d1a37470fac47e633
Guilherme França de Oliveira <guigo613@gmail.com>
2022-01-08 01:34:34 -0300

    fix

--- src/main/webapp/resources/js/meteo/main.js
+++ src/main/webapp/resources/js/meteo/main.js
@@ -15,7 +15,7 @@ const treat_values = response => {
 const set_values = () => {
     let panel = $('.panel-card').removeClass('on')
     for (const [k, v] of Object.entries(lastUpgrade)) {
-        let equip = panel.filter(`[for=${k}]`);
+        let equip = panel.filter(`#${k}, [for=${k}]`);
         for (const [k2, v2] of Object.entries(v)) {
             let card = equip.filter(`.${k2}`).addClass('on')
             if (card.hasClass('deg'))
