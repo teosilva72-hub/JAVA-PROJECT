839a9f0f406ecf2106317c85c9c225401b2b1ec1
wellingtonsc88 <wellingtonsc88@gmail.com>
2022-01-17 22:48:17 -0300

    Occ fixed

--- src/main/java/br/com/tracevia/webapp/model/occ/OcurrenceDetails2.java
+++ src/main/java/br/com/tracevia/webapp/model/occ/OcurrenceDetails2.java
@@ -12,20 +12,50 @@ public class OcurrenceDetails2 {
 		this.id = id;
 		this.campo = campo;
 		this.valores = valores;
-		this.setActive(active);
+		this.active = active;
 		
 	}
 	
-		public OcurrenceDetails2 (int id, String valores) {
-		
-		this.id = id;		
-		this.valores = valores;	
-		
-	}
+	public OcurrenceDetails2 (int id, String valores) {
 	
-		
-		public OcurrenceDetails2() {}
+	this.id = id;		
+	this.valores = valores;	
+	
+    }
+			
+	public OcurrenceDetails2() {}
+	
+	public int getId() {
+		return id;
+	}
 
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCampo() {
+		return campo;
+	}
+
+	public void setCampo(String campo) {
+		this.campo = campo;
+	}
+
+	public String getValores() {
+		return valores;
+	}
+
+	public void setValores(String valores) {
+		this.valores = valores;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
 		
 		
 }
