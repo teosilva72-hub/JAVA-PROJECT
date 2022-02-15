11768d5b8e6868cfecb43e89f200e90114f166ac
Tracevia Teste <developers.tracevia.test@gmail.com>
2022-02-15 11:50:17 -0300

    fix

--- src/main/java/br/com/tracevia/webapp/controller/global/ReportBean.java
+++ src/main/java/br/com/tracevia/webapp/controller/global/ReportBean.java
@@ -61,7 +61,7 @@ public class ReportBean {
 	public String 	jsTable, jsTableScroll, chartTitle, imageName, vAxis;	
 	
 	public boolean 	isSat = false, haveTotal, multiSheet = true, equipSheetName = false, directionsOnSheet = false, isChart = false, special = false, headerInfo = false, classHead = false, caseSensitive = false,
-			groupId = false;
+			groupId = false,  limitColumn = false;
 	
 	public String totalType = "standard";
 	public String 	module = "default";
@@ -336,6 +336,10 @@ public class ReportBean {
 	
 	public void setPeriod(int time, String step, String name) {
 		this.period.add(new String[]{String.valueOf(time), step, name});
+	}
+	
+	public void setLimit() {
+		this.limitColumn = true;
 	}
 	
 	public List<String[]> getPeriod() {
