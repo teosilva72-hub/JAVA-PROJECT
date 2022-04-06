94c348ec7d6756a2100293a6db16d010367f13f1
Mateus Silva <representante.com.sp@gmail.com>
2022-04-05 22:57:08 -0300

    one commit

--- src/main/java/br/com/tracevia/webapp/controller/occ/TuxpanOcc.java
+++ src/main/java/br/com/tracevia/webapp/controller/occ/TuxpanOcc.java
@@ -26,16 +26,32 @@ public class TuxpanOcc{
 			e.printStackTrace();
 		}
 	}
+	public boolean select() {
+		
+		boolean check = false;
+		dao = new TuxpanDAO();
+		
+		try {
+			data = dao.select(idTable);
+			System.out.println("method select > "+idTable );
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return check;
+	}
 	public boolean saveOcc() {
 		boolean check = false;
 		dao = new TuxpanDAO();
 		try {
-			check = dao.registerOcc(data);
+			check = dao.registerOcc(data, typeReport);
 			if(check == false) {
 				//message error
 			}else {
 				//message success
 				listTable();
+				//init();
 				//RequestContext.getCurrentInstance().execute("listOcc()");
 			}
 		} catch (Exception e) {
@@ -49,6 +65,7 @@ public class TuxpanOcc{
 		try {
 			listar = dao.listarOcorrencias();
 			if(listar.size() != 0) {
+				RequestContext.getCurrentInstance().execute("table()");
 				check = true;
 			}
 		} catch (Exception e) {
@@ -59,7 +76,7 @@ public class TuxpanOcc{
 		return check;
 	}
 	//variables
-	private String idTable;
+	private String typeReport, idTable;
 	private TuxpanDAO dao;
 	private TuxpanOccModel data;
 	private List<TuxpanOccModel> listar;
@@ -67,6 +84,19 @@ public class TuxpanOcc{
 	//setter and getter
 	public List<TuxpanOccModel> getListar() {
 		return listar;
+	}
+	
+	public String getIdTable() {
+		return idTable;
+	}
+	public void setIdTable(String idTable) {
+		this.idTable = idTable;
+	}
+	public String getTypeReport() {
+		return typeReport;
+	}
+	public void setTypeReport(String typeReport) {
+		this.typeReport = typeReport;
 	}
 	public void setListar(List<TuxpanOccModel> listar) {
 		this.listar = listar;
--- src/main/java/br/com/tracevia/webapp/dao/occ/TuxpanDAO.java
+++ src/main/java/br/com/tracevia/webapp/dao/occ/TuxpanDAO.java
@@ -12,12 +12,69 @@ import br.com.tracevia.webapp.model.occ.TuxpanOccModel;
 public class TuxpanDAO{
 	SQL_Tracevia conn = new SQL_Tracevia();
 	
-	public boolean registerOcc(TuxpanOccModel data) throws Exception {
+	public TuxpanOccModel select(String id) throws Exception {
+		TuxpanOccModel report = new TuxpanOccModel();
+		
+		String query = "SELECT cobro, folio_secuencial, reporte, siniestro, fecha, direccion, km_reg, km_inicial, km_final, poliza,"+
+				"hora_reg_cab, hora_arr_aju, semoviente, trabajo_cons, tiempo, neblina, vandalismo, otro, obs, tipo_veh, tipo_veh_eje,"+
+				"num_veh_inv, marca_veh_inv, tipo_veh_inv, modelo_veh_inv, color_veh_inv, placa_est_veh_inv, tel_veh_inv, num_person,"+
+				"nombre_person, edad_person, condiccion_person, hora, type_report, id FROM occ_tuxpan WHERE id ='"+id+"'";
+		try {
+			conn.start(1);
+			conn.prepare(query);
+			conn.setString(1, report.getPlz_cobro());
+			conn.setString(2, report.getFolio_sec());
+			conn.setString(3, report.getReporte());
+			conn.setString(4, report.getSiniestro());
+			conn.setString(5, report.getFecha());
+			conn.setString(6, report.getDireccion());
+			conn.setString(7, report.getKm_reg());
+			conn.setString(8, report.getKm_inicial());
+			conn.setString(9, report.getKm_final());
+			conn.setString(10, report.getPoliza());
+			conn.setString(11, report.getFecha_cab());
+			conn.setString(12, report.getHora_ajust());
+			conn.setString(13, report.getSemoviente());
+			conn.setString(14, report.getTrab_conserv());
+			conn.setString(15, report.getLluvia_granizo());
+			conn.setString(16, report.getNeblina());
+			conn.setString(17, report.getVandalismo());
+			conn.setString(18, report.getOtro());
+			conn.setString(19, report.getObs_occ());
+			conn.setString(20, report.getTipo_veh_inv());
+			conn.setString(21, report.getNum_eje_veh_inv());
+			conn.setString(22, report.getNum_tp_veh());
+			conn.setString(23, report.getMarca_tp_veh());
+			conn.setString(24, report.getTipo_tp_veh());
+			conn.setString(25, report.getModel_tp_veh());
+			conn.setString(26, report.getColor());
+			conn.setString(27, report.getPlaca_estado());
+			conn.setString(28, report.getTel());
+			conn.setString(29, report.getId_person());
+			conn.setString(30, report.getNombre());
+			conn.setString(31, report.getEdad());
+			conn.setString(32, report.getCondiciones());
+			conn.setString(33, report.getHora());
+			conn.setString(34, report.getType_report());
+			conn.setString(35, report.getId());
+			long occ = conn.executeUpdate();
+			System.out.println(occ);
+			
+		}catch (Exception e){
+			throw new Exception("Erro ao inserir dados: " + e);
+		}finally {
+			conn.close();
+		}
+		return report;
+	}
+	
+	public boolean registerOcc(TuxpanOccModel data, String type_report) throws Exception {
 		boolean saveOcc = false;
 		String query = "INSERT INTO occ_tuxpan(cobro, folio_secuencial, reporte, siniestro, fecha, direccion, km_reg, km_inicial, km_final, poliza,"+
 						"hora_reg_cab, hora_arr_aju, semoviente, trabajo_cons, tiempo, neblina, vandalismo, otro, obs, tipo_veh, tipo_veh_eje,"+
-						"num_veh_inv, marca_veh_inv, tipo_veh_inv, modelo_veh_inv, color_veh_inv, placa_est_veh_inv, tel_veh_inv, num_person, nombre_person, edad_person, condiccion_person, hora)"+
-						" VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+						"num_veh_inv, marca_veh_inv, tipo_veh_inv, modelo_veh_inv, color_veh_inv, placa_est_veh_inv, tel_veh_inv, num_person,"+
+						"nombre_person, edad_person, condiccion_person, hora, type_report)"+
+						" VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
 		
 		try {
 			conn.start(1);
@@ -55,6 +112,7 @@ public class TuxpanDAO{
 			conn.setString(31, data.getEdad());
 			conn.setString(32, data.getCondiciones());
 			conn.setString(33, data.getHora());
+			conn.setString(34, type_report);
 			long occ = conn.executeUpdate();
 			System.out.println(occ);
 			if(occ > 0) {
